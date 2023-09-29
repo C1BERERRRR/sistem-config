96a17f865bdb0614fd2129935ee09585539c3ac5
C1BERERRRR <moasupra12@gmail.com>
2023-09-29 12:50:32 +0300

    release v1

---
+++ test.py
@@ -0,0 +1,27 @@
+import platform
+import wmi
+
+# Создаем объект WMI
+wmi_obj = wmi.WMI()
+
+# Получаем информацию о видеокарте
+for gpu in wmi_obj.Win32_VideoController():
+    print("Название видеокарты: ", gpu.Caption)
+    print("Память видеокарты: ", gpu.AdapterRAM, "байт")
+    print("Тип видеокарты: ", gpu.VideoProcessor)
+
+def get_system_info():
+    processor = platform.processor()
+    video_card = platform.system()
+    motherboard = platform.machine()
+    
+    return processor, video_card, motherboard
+
+def main():
+    processor, video_card, motherboard = get_system_info()
+    
+    print("Processor:", processor)
+    print("Motherboard:", motherboard)
+
+if __name__ == "__main__":
+    main()
